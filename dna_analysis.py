ec2ca777a9626dc7dcd8836e7d8ec2a05886e728
spehan01 <hcrspencer@gmail.com>
2013-10-31 14:26:22 -0700

    homework 4

--- dna_analysis.py
+++ dna_analysis.py
@@ -54,6 +54,10 @@ total_count = 0
 # Number of G and C nucleotides seen so far.
 gc_count = 0
 at_count = 0
+a_count = 0
+t_count = 0
+g_count = 0
+c_count = 0
 
 # for each base pair in the string,
 for bp in seq:
@@ -61,17 +65,44 @@ for bp in seq:
     total_count = total_count + 1
 
     # next, if the bp is a G or a C,
-    if bp == 'C' or bp == 'G':
+    if bp == 'C' or bp == 'G':      
         # increment the count of gc
         gc_count = gc_count + 1
-    elif bp == 'A' or bp == 'T': 
+    elif bp == 'A' or bp == 'T':
         at_count = at_count + 1
-
+    if bp == 'A':
+        a_count = a_count + 1
+    elif bp == 'T':
+        t_count = t_count + 1
+    elif bp == 'G':
+        g_count = g_count + 1
+    elif bp == 'C':
+        c_count = c_count +1   
+              
+sum_count = a_count + t_count + g_count + c_count
+ratio = float(at_count) / float(gc_count)
 
 # divide the gc_count by the total_count
-gc_content = float(gc_count) / total_count
-at_content = float(at_count) / total_count
+gc_content = float(gc_count) / sum_count
+at_content = float(at_count) / sum_count
+
+if gc_content >= float(0.6):
+    gc_class = 'High GC content'
+elif gc_content <= float(0.4):
+    gc_class = 'Low GC content'
+else:
+    gc_class = 'Moderate GC content'
+print gc_class
 
 # Print the answer
 print 'GC-content:', gc_content
 print 'AT_content:', at_content
+print 'A_count:', a_count
+print 'T_count:', t_count
+print 'G_count:', g_count
+print 'C_count:', c_count
+print 'Sum_count:', sum_count
+print 'Total_count:', total_count
+print 'seg length:', len(seq)
+print 'AT/GC Ratio:', ratio
+print 'GC Classification:', gc_class
